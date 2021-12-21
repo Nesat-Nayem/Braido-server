11ff7c002885e06129513ce0e276b007f995fef7
Nesat-Nayem <nesatnayem042@gmail.com>
2021-12-21 23:42:07 +0600

    code filter

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET)
-
 
 const port = process.env.PORT ||  5000;
 
@@ -15,19 +12,9 @@ const port = process.env.PORT ||  5000;
 app.use(cors());
 app.use(express.json());
 
-
-// watch side user name and pass
-
-// user name: watch-side
-// pass: lYvbnVVdwFhOvBkv
-
-// watch side user name and pass
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hty68.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
 
 client.connect((err) => {
   const servicesCollection = client.db("WatchStore").collection("services");
@@ -48,13 +35,6 @@ client.connect((err) => {
     res.send(result);
   });
 
-  // delete api
-  app.delete('/allServices/:id', async(req,res) =>{
-    const id = req.params.id;
-    const query = {_id:ObjectId(id)};
-    const result = await servicesCollection.deleteOne(query);
-    res.json(result)
-})
 
   // single service
   app.get("/singleService/:id", async (req, res) => {
@@ -65,30 +45,13 @@ client.connect((err) => {
     res.send(result[0]);
     console.log(result);
   });
-  // get services use id
-  app.get("/allServices/:id", async (req, res) => {
-    console.log(req.params.id);
-    const result = await servicesCollection
-      .find({ _id: ObjectId(req.params.id) })
-      .toArray();
-    res.send(result[0]);
-    console.log(result);
-  });
-
+  
   // insert order
 
   app.post("/addOrders", async (req, res) => {
     const result = await ordersCollection.insertOne(req.body);
     res.send(result);
   });
-
-  // app.get("/myBookings/:id", async (req, res) => {
-  //   const id = req.params.id;
-  //   const query = {_id: ObjectId(id)};
-  //   const result = await ordersCollection.findOne(query);
-  //   res.json(result);
-  // })
-
 
   app.get("/myOrder/:email", async (req, res) => {
     console.log(req.params.email);
@@ -106,47 +69,11 @@ client.connect((err) => {
     res.json(result);
   });
 
-  // review
-  app.post("/addReview", async (req, res) => {
-    const result = await reviewCollection.insertOne(req.body);
-    res.send(result);
-  });
-
-  // get review
-
-  app.get("/myReview", async (req,res) =>{
-    console.log("hello review")
-    const result = await reviewCollection.find({}).toArray();
-    res.send(result)
-  })
-
   app.post("/addUserInfo", async (req, res) => {
     console.log("req.body");
     const result = await usersCollection.insertOne(req.body);
     res.send(result);
     console.log(result);
-  });
-
-  //  make admin
-
-  app.put("/makeAdmin", async (req, res) => {
-    const filter = { email: req.body.email };
-    const result = await usersCollection.find(filter).toArray();
-    if (result) {
-      const documents = await usersCollection.updateOne(filter, {
-        $set: { role: "admin" },
-      });
-      console.log(documents);
-    }
-  });
-
-  // check admin 
-  app.get("/checkAdmin/:email", async (req, res) => {
-    const result = await usersCollection
-      .find({ email: req.params.email })
-      .toArray();
-    console.log(result);
-    res.send(result);
   });
 
   /// all order
@@ -168,19 +95,6 @@ client.connect((err) => {
     res.json(result)
   })
 
-  // status update
-  app.put("/statusUpdate/:id", async (req, res) => {
-    const filter = { _id: ObjectId(req.params.id) };
-    console.log(req.params.id);
-    const result = await ordersCollection.updateOne(filter, {
-      $set: {
-        status: req.body.status,
-      },
-    });
-    res.send(result);
-    console.log(result);
-  });
-
   
   app.post("/create-payment-intent", async(req, res) =>{
     const paymentInfo = req.body;
@@ -193,23 +107,7 @@ client.connect((err) => {
     res.json({ clientSecret: paymentIntent.client_secret })
   })
 
-//   app.post('/create-payment-intent', async (req, res) => {
-//     const paymentInfo = req.body;
-//     const amount = paymentInfo.Price * 100;
-//     const paymentIntent = await stripe.paymentIntents.create({
-//         currency: 'usd',
-//         amount: amount,
-//         payment_method_types: ['card']
-//     });
-//     res.json({ clientSecret: paymentIntent.client_secret })
-// })
-
-
-
-
 });
-
-
 
 app.get("/", (req, res) => {
   res.send("watch store server is live");
